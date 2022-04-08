e65452d1130a0ef1e30c28e5a6e3fb605f159bba
jcccc4 <jcsacra4@gmail.com>
2022-04-08 17:55:39 +0800

    fixed footer and added (+add book) functionality

--- scripts.js
+++ scripts.js
@@ -7,7 +7,42 @@ function Book(title, author, pages) {
 }
 
 function addBookToLibrary() {
-    // do stuff here
-    let newBook =
-        myLibrary.push(newBook);
+    const modal = document.getElementById("myModal");
+    let btn = document.getElementById("book");
+    const span = document.getElementsByClassName("close")[0];
+    const inputtedData = document.getElementsByClassName("inputtedData");
+    const library = document.getElementsByClassName("bookLibrary");
+    console.log(library);
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+    };
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    document.getElementById("sub-btn").addEventListener("click", function (event) {
+        event.preventDefault();
+        const node = document.createTextNode("This is new.");
+        const items = document.createElement("div");
+        modal.style.display = "none";
+
+        myLibrary.push(new Book(inputtedData[0].value, inputtedData[1].value, inputtedData[2].value));
+
+        items.appendChild(node);
+        items.className = "bookItems";
+        library[0].appendChild(items);
+        console.log(library[0]);
+
+    });
+
 }
+
+
+
+addBookToLibrary();
