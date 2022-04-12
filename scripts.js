50782708ce35f56909d7d70800d78ec303cf1d18
jcccc4 <jcsacra4@gmail.com>
2022-04-12 18:54:39 +0800

    finished functionality

--- scripts.js
+++ scripts.js
@@ -11,22 +11,17 @@ if (!localStorage.getItem('bookList')) {
     localStorage.setItem('bookList', JSON.stringify([]));
 }
 let bookList = localStorage.getItem('bookList');
-let k = JSON.parse(bookList);
 var myLibrary = JSON.parse(bookList);
-console.log(k, 21312);
 
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
-
-
-    console.log(library);
-
     btn.onclick = function () {
         modal.style.display = "block";
     };
@@ -38,39 +33,88 @@ function addBookToLibrary() {
             modal.style.display = "none";
         }
     };
+}
 
-    document.getElementById("sub-btn").addEventListener("click", function (event) {
-
+function validateForm() {
+    document.getElementById("sub-btn").addEventListener("click", function () {
         modal.style.display = "none";
+        let x = document.forms["myForm"];
+        console.log(myLibrary);
+        if (x[0].value.length === 0 || x[1].value.length === 0 || x[2].value.length === 0) {
+            alert("All input must be filled out");
 
-        console.log(bookList);
-
-        myLibrary.push(new Book(inputtedData[0].value, inputtedData[1].value, inputtedData[2].value));
-        localStorage.setItem("bookList", JSON.stringify(myLibrary));
-
+        } else {
+            myLibrary.push(new Book(inputtedData[0].value, inputtedData[1].value, inputtedData[2].value, inputtedData[3].checked));
+            localStorage.setItem("bookList", JSON.stringify(myLibrary));
+            location.reload();
+        }
     });
-
 }
 
 function Library() {
-    console.log(JSON.parse(bookList).length, 'jk');
+    console.log(myLibrary);
     for (let i = 0; i < JSON.parse(bookList).length; i++) {
         const items = document.createElement("div");
+        const closeItems = document.createElement("span");
         items.className = "bookItems";
-
+        items.appendChild(closeItems);
+        closeItems.innerHTML = "<span class='closeItems'>&times;</span>";
         for (let loop in myLibrary[i]) {
             const description = document.createElement("div");
             items.appendChild(description);
             description.className = "description";
-            console.log(myLibrary);
-            description.innerHTML = `${myLibrary[i][loop]}`;
+            if (loop === 'read') {
+                const buttonRead = document.createElement("button");
+                items.appendChild(buttonRead);
+                buttonRead.className = 'buttonRead';
+                if (myLibrary[i][loop] === true) {
+                    buttonRead.className = 'readClick buttonRead';
+                    buttonRead.innerText = "Read";
+                } else {
+                    buttonRead.className = 'readClick buttonNotRead';
+                    buttonRead.innerText = "Not Read";
+                }
+            } else {description.innerHTML = `${myLibrary[i][loop]}`;};
         }
 
         library[0].appendChild(items);
-        console.log(library[0]);
+    }
+
+}
+function onRead() {
+    const readClick = document.getElementsByClassName("readClick");
+    for (let click in myLibrary) {
+        readClick[click].addEventListener('click', () => {
+            if (myLibrary[click].read === true) {
+                myLibrary[click].read = false;
+                localStorage.setItem("bookList", JSON.stringify(myLibrary));
+                location.reload();
+            } else {
+                myLibrary[click].read = true;
+                localStorage.setItem("bookList", JSON.stringify(myLibrary));
+                location.reload();
+            }
+        });
+    }
+}
+function closeItem() {
+    const removeItems = document.getElementsByClassName("closeItems");
+
+    for (let rep in myLibrary) {
+
+        removeItems[rep].addEventListener('click', function () {
+
+            myLibrary.splice(rep, 1);
+
+            localStorage.setItem("bookList", JSON.stringify(myLibrary));
+            location.reload();
+        });
     }
 }
 
 
 addBookToLibrary();
 Library();
+validateForm();
+closeItem();
+onRead();
